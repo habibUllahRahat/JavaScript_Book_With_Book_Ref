d5f4e506edb6ec7cc487eff626be65b83e8e2bdb
Habib Ullah Rahat <rahat09.hur@gmail.com>
2023-06-26 23:39:15 +0600

    ' '

--- Codes/1stChapter/behaindTheScene.js
+++ Codes/1stChapter/behaindTheScene.js
@@ -15,3 +15,15 @@ _________________
  * Whenever a function execute happens or function call happens then it create another execution  context.
  * In every execution context get there own "this" keyword. 
  */
+
+/**JavaSript Engine create Global Execution Context when it execute code.
+ * And in JavaScript default Global Execution Context is "window" object.
+ */
+var a =10;
+console.log("value of a is : "+a);
+console.log("value of window.a is : "+window.a);
+
+/**New execution context : Whenever a function call or creation happen a new execution context is and this context has two phase:
+ * Creation Phase.
+ * Execution Phase.
+ */
